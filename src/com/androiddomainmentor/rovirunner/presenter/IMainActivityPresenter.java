8fb1e443655a60f2f922e6000a66f2e9571e99b3
Andrew Wood <awood82@gmail.com>
2013-09-20 17:32:17 -0700

    Forgot a file in the last commit

--- src/com/androiddomainmentor/rovirunner/presenter/IMainActivityPresenter.java
+++ src/com/androiddomainmentor/rovirunner/presenter/IMainActivityPresenter.java
@@ -1,11 +1,12 @@
 package com.androiddomainmentor.rovirunner.presenter;
 
+import android.content.Context;
 import android.widget.ExpandableListAdapter;
 
 public interface IMainActivityPresenter
 {
 
-    void playLocalMusic();
+    void playLocalMusic(Context context);
 
     /*!
      * returns an adapter that is hooked up to a list of streaming sources
